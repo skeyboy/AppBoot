3d22fa51faadaea9823ef2a8e329f95bc5e18264
sk <lylapp@163.com>
2021-08-21 19:39:41 +0800

    Initial Commit

---
+++ AppBoot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppBoot
+//
+//  Created by sk on 2021/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
